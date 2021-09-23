8e9159daace11aed0faa08f72d9c3ec485f8dec9
Martin Frederiksen <zyjinxx@hotmail.com>
2021-09-23 13:37:25 +0200

    Added isbn validater to webapi

--- mini-project-1/MiniProject1.WebApi/Controllers/StudentController.cs
+++ mini-project-1/MiniProject1.WebApi/Controllers/StudentController.cs
@@ -30,5 +30,17 @@ namespace MiniProject1.WebApi.Controllers
         {
             return await GrpcClient.GetAll();
         }
+
+        [HttpGet("validateISBN10/{isbn}")]
+        public async Task<bool> GetStudent(int isbn)
+        {
+            return await SoapConnector.ISBN10Validator(isbn);
+        }
+
+        [HttpGet("validateISBN13/{isbn}")]
+        public async Task<bool> GetStudent(int isbn)
+        {
+            return await SoapConnector.ISBN13Validator(isbn);
+        }
     }
 }
