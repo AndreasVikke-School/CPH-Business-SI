887be463edaa73727ae4158b7132739834e74520
andreasvikke <azv@efio.dk>
2021-11-08 15:39:18 +0100

    Fix PYthon

--- mini-project-3/service_cache/main.py
+++ mini-project-3/service_cache/main.py
@@ -1,22 +1,20 @@
 import json
+import time
 import redis
 from kafka import KafkaConsumer
 
-kafka_consumer = KafkaConsumer(group_id='cache_service', bootstrap_servers='kafka:9092')
-kafka_consumer.subscribe(['loan-reply'])
+kafka_consumer = KafkaConsumer('loan-reply',
+                         group_id='chache_service',
+                         bootstrap_servers=['localhost:9092'])
 
 while True:
-    try:
-        message = kafka_consumer.poll(timeout_ms=1000)
+    msg_pack = kafka_consumer.poll(timeout_ms=500)
 
-        from_kafka = json.load(message.value().decode())
+    for tp, message in msg_pack.items():
+        from_kafka = json.loads(message[0].value)
 
-        r = redis.StrictRedis(host='redis', port=6379, db=0, charset='utf-8', decode_responses=True, password='P@ssword!')
+        r = redis.StrictRedis(host='localhost', port=6379, db=0, charset='utf-8', decode_responses=True, password='P@ssword!')
         r.incr(from_kafka["userId"], 1)
         for k, v in from_kafka.items():
             r.hset('{0}:{1}'.format(from_kafka["userId"], r.get(from_kafka["userId"])), k, v)
         print(r.hgetall('{0}:{1}'.format(from_kafka["userId"], r.get(from_kafka["userId"]))))
-    except:
-        print('Failed')
-    finally:
-        kafka_consumer.close()
