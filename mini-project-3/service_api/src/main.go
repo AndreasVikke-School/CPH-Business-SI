d122571d51cb52b5ad29cc1fde43580b44d42567
andreasvikke <azv@efio.dk>
2021-11-08 11:40:36 +0100

    Added /loan/loan-request endpoint

--- mini-project-3/service_api/src/main.go
+++ mini-project-3/service_api/src/main.go
@@ -1,34 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/segmentio/kafka-go"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+const (
+	topic          = "loan-request"
+	broker1Address = "kafka:9092"
+)
+
+type request struct {
+	Amount int64 `json:"amount"`
+	Start  int32 `json:"startMonth"`
+	End    int32 `json:"endMonth"`
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+func requestLoans(c *gin.Context) {
+	var newRequest request
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	if err := c.BindJSON(&newRequest); err != nil {
+		return
+	}
+	jsonRequest, errR := json.Marshal(newRequest)
+	if errR != nil {
+		return
+	}
+
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{broker1Address},
+		Topic:   topic,
+	})
+
+	err := w.WriteMessages(c, kafka.Message{
+		Key:   []byte("loan-request"),
+		Value: []byte(jsonRequest),
+	})
+	if err != nil {
+		panic("could not write message " + err.Error())
+	}
+
+	c.IndentedJSON(http.StatusOK, newRequest)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	router.POST("/loan/request", requestLoans)
 
 	router.Run("0.0.0.0:8080")
 }
