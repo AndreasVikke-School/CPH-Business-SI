8ea435b1128456ea1d3b97d6978497e5986761e0
andreasvikke <azv@efio.dk>
2021-11-08 12:21:29 +0100

    Added /loan/select endpoint

--- mini-project-3/service_api/src/main.go
+++ mini-project-3/service_api/src/main.go
@@ -6,17 +6,32 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
+	"github.com/streadway/amqp"
 )
 
 const (
 	topic          = "loan-request"
 	broker1Address = "kafka:9092"
+	queue          = "Loan Queue"
+	rabbitmq       = "amqp://admin:P@ssword!@rabbitmq:5672/"
 )
 
 type request struct {
 	Amount int64 `json:"amount"`
 	Start  int32 `json:"startMonth"`
 	End    int32 `json:"endMonth"`
+}
+
+type lselect struct {
+	Id       int64   `json:"id"`
+	BankId   int64   `json:"bankId"`
+	Cpr      string  `json:"cpr"`
+	Name     string  `json:"name"`
+	Email    string  `json:"email"`
+	Amount   int64   `json:"amount"`
+	MToP     int32   `json:"monthToPay"`
+	Interest float32 `json:"interest"`
+	AOP      float32 `json:"aop"`
 }
 
 func requestLoans(c *gin.Context) {
@@ -46,9 +61,48 @@ func requestLoans(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, newRequest)
 }
 
+func selectLoan(c *gin.Context) {
+	var newSelect lselect
+
+	if err := c.BindJSON(&newSelect); err != nil {
+		return
+	}
+	jsonSelect, errR := json.Marshal(newSelect)
+	if errR != nil {
+		return
+	}
+
+	conn, err := amqp.Dial(rabbitmq)
+	if err != nil {
+		panic("Failed " + err.Error())
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		panic("Failed " + err.Error())
+	}
+	defer ch.Close()
+
+	err = ch.Publish(
+		"",           // exchange
+		"Loan Queue", // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(jsonSelect),
+		})
+	if err != nil {
+		panic("Failed " + err.Error())
+	}
+
+	c.IndentedJSON(http.StatusOK, newSelect)
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/loan/request", requestLoans)
+	router.POST("/loan/select", selectLoan)
 
 	router.Run("0.0.0.0:8080")
 }
